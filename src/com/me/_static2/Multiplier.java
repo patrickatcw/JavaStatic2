72a21a938f1f7acad11f9a330f73d7d6bb78290a
patrickatcw <pj8392@yahoo.com>
2018-05-02 10:48:01 -0400

    made multiplier class, now to main

---
+++ src/com/me/_static2/Multiplier.java
@@ -0,0 +1,16 @@
+package com.me._static2;
+
+//step 1 class
+public class Multiplier {
+
+    //step 2 variable
+    public static int multiplier = 7;
+
+    //step 3 method
+    public static int multiply(int number){
+        return number * multiplier;
+
+        //step 4 to main
+    }
+
+}
