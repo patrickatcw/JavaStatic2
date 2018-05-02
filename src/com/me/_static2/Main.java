17198f51d94038642a5f3bbb9335609ea1b74b6a
patrickatcw <pj8392@yahoo.com>
2018-05-02 10:52:57 -0400

    ran in main, program good

--- src/com/me/_static2/Main.java
+++ src/com/me/_static2/Main.java
@@ -1,9 +1,22 @@
 package com.me._static2;
 
+import static com.me._static2.Multiplier.multiplier;
+import static com.me._static2.Multiplier.multiply;
+//note; imports required due to static
+
 public class Main {
 
     public static void main(String[] args) {
 
+        //step 4 calling method
+        int answer = multiply(6);
+        System.out.println("The answer is " + answer);
+        System.out.println("Multiplier is " + multiplier);
+        //run
+
+        //results;
+        /*  The answer is 42
+            Multiplier is 7     */
     }
 
 }
